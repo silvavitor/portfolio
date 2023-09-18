e49c81a831f96ce4d7bfd142d0f8d2c8ee2e023f
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2023-09-18 13:40:48 -0300

    feat: add gradient circles on image corner

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,26 @@
 import Image from "next/image";
 
-import vitor from "@/public/images/vitor.jpg";
 import Button from "@/components/Button";
 import Link from "next/link";
+
+import vitor from "@/public/images/vitor.jpg";
+import circles from "@/public/images/circles.png";
 
 export default function Home() {
   return (
     <div className="py-12">
       {/* About */}
       <div className="flex items-center justify-center">
-        <Image src={vitor} height={420} alt="Vitor Fernando da Silva" />
+        <div className="h-[420px] relative top-0 left-0">
+          <div className="absolute bottom-[357px] left-[-79px]">
+            <Image src={circles} height={125} alt="circles" className="rounded-lg" />
+          </div>
+          <Image src={vitor} alt="Vitor Fernando da Silva" className="h-full w-fit rounded-md" />
+          <div className="absolute top-[357px] right-[-79px]">
+            <Image src={circles} height={125} alt="circles" className="rounded-lg" />
+          </div>
+        </div>
+
         <div className="ml-6 flex flex-col max-w-lg">
           <h1 className="text-4xl font-bold leading-tight">Olá, me chamo Vitor e sou <span className="bg-black text-neutral-100">Desenvolvedor Full-Stack</span></h1>
           <p className="mt-2 text-sm">Sou um desenvolvedor do Rio Grande do Sul, Brasil. Possuo 4 anos de experiência comercial, porém, tenho contato com programação há 8 anos. <br />Estou aberto a novas oportunidades e projetos.</p>
