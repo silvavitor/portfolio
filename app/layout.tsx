a0bb4ef8efaa6179a9a00480cdd60bc1a4173a40
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2023-09-18 12:11:56 -0300

    feat: remove next files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Header from '@/components/Header'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-neutral-100 mx-auto max-w-6xl`}>
+        <Header />
+        {children}
+      </body>
     </html>
   )
 }
