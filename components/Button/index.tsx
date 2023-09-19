e2a7015900ecf492fe17fbb6cfa849ce606d5adb
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2023-09-19 12:54:20 -0300

    feat: change button behavior and style

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,16 +1,24 @@
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
+import Image from "next/image";
+
 type ButtonProps = {
-  dark?: Boolean,
-  children: any
+  small?: Boolean,
+  icon?: StaticImport | string,
+  children: String
 }
 
-export default function Button({ dark = false, children }: ButtonProps) {
+export default function Button({ small = false, icon = '', children }: ButtonProps) {
+  const hasIcon = icon !== '';
   return (
-    <button className={`px-8 py-3 font-bold rounded-md drop-shadow-md hover:drop-shadow-xl hover:opacity-80 hover:scale-[1.02] transition duration-300 
-      ${dark
-        ? 'bg-black text-neutral-100'
-        : 'bg-neutral-100 text-black border border-black'}`}
+    <button className={`group bg-neutral-100 text-black border border-black font-bold rounded-md drop-shadow-md hover:drop-shadow-xl hover:bg-black hover:text-neutral-100 transition duration-200 
+      ${small
+        ? 'px-4 py-2 text-xs'
+        : 'px-8 py-3 '}`}
     >
-      {children}
+      <div className="flex items-center justify-center ">
+        {hasIcon && (<Image src={icon} alt="icon" height={18} className="mr-1 group-hover:invert" />)}
+        {children}
+      </div>
     </button>
   );
 }
