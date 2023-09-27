f24273472713a31b2a229c46ce11585c159f4e96
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2023-09-27 20:30:12 -0300

    feat: projects page responsive

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -19,15 +19,15 @@ export default function Projects() {
     <div className="flex flex-col items-center mx-auto mb-32">
       <h1 className="text-3xl font-bold">Projetos</h1>
       <div className="mt-3 text-sm flex flex-col items-center">
-        <div className="space-x-2">
-          <button key={'all'} className="px-2 py-1 rounded-lg bg-neutral-200 cursor-pointer" onClick={filterTechs} id={'all'}>Todos</button>
+        <div className="space-x-2 space-y-2 text-center">
+          <button key={'all'} className="m-1 px-2 py-1 rounded-lg bg-neutral-200 cursor-pointer" onClick={filterTechs} id={'all'}>Todos</button>
           {techs?.map((tech, index) => (
-            <button key={index} className="px-2 py-1 rounded-lg bg-neutral-200 cursor-pointer" onClick={filterTechs} id={tech}>{tech}</button>
+            <button key={index} className="m-1 px-2 py-1 rounded-lg bg-neutral-200 cursor-pointer" onClick={filterTechs} id={tech}>{tech}</button>
           ))}
         </div>
 
       </div>
-      <div className="mt-10 grid grid-cols-3 gap-x-[70px] gap-y-16">
+      <div className="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-x-[70px] gap-y-8 md:gap-y-12 lg:gap-y-16">
         {filteredProjects?.map((project, index) => (
           <ProjectCard key={index} project={project} />
         ))}
