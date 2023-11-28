d233af11c92e31b555e7fcc5e75be9a407e77acc
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2023-11-27 23:24:15 -0300

    fix: translate 20px instead of 30px

--- components/ProjectCard/index.tsx
+++ components/ProjectCard/index.tsx
@@ -28,7 +28,7 @@ export default function ProjectCard({ project }: ProjectCardProps) {
       ref={ref}
       onClick={() => push(`/projects/${project.id}`)}
       className={`mt-8 md:mt-0 mx-1 w-64 h-96 py-5 px-3 cursor-pointer bg-white flex flex-col rounded-md shadow-md hover:shadow-xl transition duration-200
-        ${isInView ? 'animate-bottomUpFadeIn' : 'translate-y-[30px]'}
+        ${isInView ? 'animate-bottomUpFadeIn' : 'translate-y-5'}
       `}
     >
       <Image src={`/images/${project.images[0]}`} alt={project.title} width={300} height={300} className="rounded-md h-32" />
