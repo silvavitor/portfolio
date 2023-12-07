0362110bf77e62167c2b6d470458e8f68afb6bbc
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2023-12-07 10:17:19 -0300

    feat: change image style in project card

--- components/ProjectCard/index.tsx
+++ components/ProjectCard/index.tsx
@@ -27,34 +27,36 @@ export default function ProjectCard({ project }: ProjectCardProps) {
     <div
       ref={ref}
       onClick={() => push(`/projects/${project.id}`)}
-      className={`mt-8 md:mt-0 mx-1 w-64 h-96 py-5 px-3 cursor-pointer bg-white flex flex-col rounded-md shadow-md hover:shadow-xl transition duration-200
+      className={`mt-8 md:mt-0 mx-1 w-64 h-96 cursor-pointer bg-white flex flex-col rounded-md shadow-md hover:shadow-xl transition duration-200
         ${isInView ? 'animate-bottomUpFadeIn' : 'translate-y-5'}
       `}
     >
-      <Image src={`/images/${project.images[0]}`} alt={project.title} width={300} height={300} className="rounded-md h-32" />
+      <Image src={`/images/${project.images[0]}`} alt={project.title} width={300} height={300} className="rounded-t-md" />
 
-      <h2 className="font-bold text-lg mt-2">{project.title}</h2>
+      <div className="px-3 mb-30">
+        <h2 className="font-bold text-lg mt-2">{project.title}</h2>
 
-      <div className="mt-3 text-xs space-x-1">
-        {techs.map((tech, index) => (
-          <span key={index} className="px-2 py-1 rounded-lg bg-neutral-200">{tech}</span>
-        ))}
-        <span className={"px-2 py-1 rounded-lg bg-neutral-200 " + (project.techs.length > 3 ? '' : 'hidden')}>+{project.techs.length - 3}</span>
-      </div>
+        <div className="mt-3 text-xs space-x-1">
+          {techs.map((tech, index) => (
+            <span key={index} className="px-2 py-1 rounded-lg bg-neutral-200">{tech}</span>
+            ))}
+          <span className={"px-2 py-1 rounded-lg bg-neutral-200 " + (project.techs.length > 3 ? '' : 'hidden')}>+{project.techs.length - 3}</span>
+        </div>
 
-      <div className="mt-4 h-24">
-        <p className="leading-5 text-xs line-clamp-4">{project.description[language]}</p>
-      </div>
+        <div className="mt-4 h-24">
+          <p className="leading-5 text-xs line-clamp-4">{project.description[language]}</p>
+        </div>
 
-      <div className="mt-5 flex justify-evenly">
-        <a href={project.repo} target="_blank" className={project.repo ? 'block' : 'hidden'}>
-          <Button small icon={github}>Repo</Button>
-        </a>
+        <div className="mt-2 flex justify-evenly">
+          <a href={project.repo} target="_blank" className={project.repo ? 'block' : 'hidden'}>
+            <Button small icon={github}>Repo</Button>
+          </a>
 
-        <a href={project.live} target="_blank" className={project.live ? 'block' : 'hidden'}>
-          <Button small icon={eye}>Live</Button>
-        </a>
-      </div>
-    </div >
+          <a href={project.live} target="_blank" className={project.live ? 'block' : 'hidden'}>
+            <Button small icon={eye}>Live</Button>
+          </a>
+        </div>
+      </div >
+    </div> 
   );
 }
